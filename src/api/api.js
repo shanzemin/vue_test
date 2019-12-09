e548ed2bebdfedd007cd3b9a6e0c48bff80e2b29
shanzm <nigegeswo520@qq.com>
2019-12-09 12:27:32 +0800

    添加样式，完善注册界面

--- src/api/api.js
+++ src/api/api.js
@@ -1,10 +1,25 @@
 import axios from 'axios'
 
+// 请求拦截器
+axios.interceptors.request.use(function (config) {
+  // 给每个请求里面添加token信息
+  config.headers.Authorization = 'Bearer ' + localStorage.getItem('Authorization')
+  return config
+}, function (error) {
+  return Promise.reject(error)
+})
+
 export default {
   login (params) {
     return axios.post('/api/users/login', params)
   },
   register (params) {
     return axios.post('/api/users/', params)
+  },
+  me () {
+    return axios.get('/api/users/me')
+  },
+  checkName (name) {
+    return axios.get(`/api/users/checkName?name=${name}`)
   }
 }
--- src/components/home/home.vue
+++ src/components/home/home.vue
@@ -1,6 +1,8 @@
 <template>
   <div>
-    欢迎用户访问
+    <span>
+      欢迎用户{{username}}访问
+    </span>
   <el-row>
     <el-button type="warning" @click="logout()" plain>注销</el-button>
   </el-row>
@@ -9,12 +11,26 @@
 
 <script>
 export default {
+  data () {
+    return {
+      username: ''
+    }
+  },
+  mounted(){
+    this.me()
+  },
   methods: {
     logout() {
       localStorage.removeItem('Authorization')
       this.$router.push({
         path: '/login'
       })
+    },
+    me() {
+      this.$http.me()
+        .then(user => {
+          this.username = user.data.data.name
+        })
     }
   }
 }
